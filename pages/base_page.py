b0f11a71d43d4a48a3de36213ed83e87b0cdc4c4
zemol <zemol@users.noreply.github.com>
2021-04-08 11:03:29 +0200

    Method is_element_presented has been added. Fixed the error in the method open (removed extra spaces)

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,19 @@
-class BasePage():
-    """class for Base Page."""
+from selenium.common.exceptions import NoSuchElementException
 
-    def __init__(self, browser, url):
-        #super(BasePage, self).__init__()
+class BasePage():
+    """basic object for web-pages"""
+
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
-        def open(self):
-            self.browser.get(self.url)
+    def open(self):
+        self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
