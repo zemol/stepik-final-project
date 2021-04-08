6e436ad75722f6b884f376c25ecb040a7b79a275
zemol <zemol@users.noreply.github.com>
2021-04-08 11:06:27 +0200

    Method go_to_login_page has been changed on to should_be_login_link

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,11 @@
-link = "http://selenium1py.pythonanywhere.com/"
+from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+
+    page = MainPage(browser, link)
+
+    page.open()
+
+    page.should_be_login_link()
+    #page.go_to_login_page()
